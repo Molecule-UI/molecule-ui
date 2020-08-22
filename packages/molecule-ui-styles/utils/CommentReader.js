6702fd537adb45de0d46792c08abec9110f8a687
Rushil dhinoja <mr.rushil17@gmail.com>
2020-08-22 17:04:29 +0530

    Documentation Update
    Tried something to get out the documentation from scss to css to JSON using comment-parser

---
+++ packages/molecule-ui-styles/utils/CommentReader.js
@@ -0,0 +1,52 @@
+const parse = require('comment-parser');
+const fs = require('fs');
+const path = require('path');
+const docsPath = path.resolve(__dirname, '../docs/documentation.js');
+const cssPath = path.resolve(__dirname, '../build/index.css');
+
+const cssData = fs.readFileSync(cssPath, 'utf-8');
+
+const comments = parse(cssData, { dotted_names: false, join: true });
+
+const modifiedData = comments.map((el) => {
+    delete el.description;
+    delete el.line;
+    delete el.source;
+
+    el.tags.map((el) => {
+        delete el.type;
+        delete el.source;
+        delete el.line;
+    });
+
+    if (el.tags[el.tags.length - 1].name === 'list') {
+        const object = {};
+        const values = el.tags[el.tags.length - 3].description.split(' ');
+        values.forEach((value) => {
+            object[value] = value;
+        });
+
+        el.tags[el.tags.length - 3].description = object;
+    }
+
+    if (el.tags[el.tags.length - 1].name === 'map') {
+        const object = {};
+        const values = el.tags[el.tags.length - 3].description
+            .replace('(', '')
+            .replace(')', '')
+            .split(',');
+        values.map((value) => {
+            const keyValuePair = value.replace(/"/g, '').replace(/ /g, '').split(':');
+            object[keyValuePair[0]] = keyValuePair[1];
+        });
+
+        el.tags[el.tags.length - 3].description = object;
+    }
+
+    return el;
+});
+
+const writeData = `module.exports=${JSON.stringify(modifiedData)}`;
+(async (writeData) => {
+    await fs.writeFileSync(docsPath, writeData, { encoding: 'utf-8' });
+})(writeData);
