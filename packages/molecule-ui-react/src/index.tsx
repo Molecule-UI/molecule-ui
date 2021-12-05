a4267e1a114ebcfb8bc6808ab2bdd34adab246d4
Rushil-Dhinoja <mr.rushil17@gmail.com>
2021-12-05 22:40:57 +0530

    Profile Card v0.1 Complete

--- packages/molecule-ui-react/src/index.tsx
+++ packages/molecule-ui-react/src/index.tsx
@@ -2,3 +2,4 @@ import "@molecule-ui/styles/build/global.css";
 export { default as Button } from "./Buttons/Button";
 export { default as Alert } from "./Alert";
 export { default as ButtonGroup } from "./Buttons/ButtonGroup";
+export { default as ProfileCard } from "./Cards/ProfileCard";
